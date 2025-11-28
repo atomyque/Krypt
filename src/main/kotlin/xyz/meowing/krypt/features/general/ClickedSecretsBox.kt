44cf91ddbe3c8b663c8f1700ed2fe3b2c8525302
aurielyn <aurielyn.dev@gmail.com>
2025-11-28 12:13:32 +0530

    Misc

--- src/main/kotlin/xyz/meowing/krypt/features/general/ClickedSecretsBox.kt
+++ src/main/kotlin/xyz/meowing/krypt/features/general/ClickedSecretsBox.kt
@@ -2,7 +2,6 @@ package xyz.meowing.krypt.features.general
 
 import net.minecraft.core.BlockPos
 import net.minecraft.world.phys.AABB
-import xyz.meowing.knit.api.KnitClient.client
 import xyz.meowing.krypt.annotations.Module
 import xyz.meowing.krypt.api.dungeons.enums.map.Room
 import xyz.meowing.krypt.api.location.SkyBlockIsland
@@ -20,19 +19,20 @@ import java.awt.Color
 object ClickedSecretsBox : Feature(
     "secretBox",
     island = SkyBlockIsland.THE_CATACOMBS
-){
+) {
     override fun addConfig() {
-        ConfigManager.addFeature(
-            "Clicked secret boxes",
-            "Draw a box on clicked secrets",
-            "General",
-            ConfigElement(
-                "secretBox",
-                ElementType.Switch(false)
+        ConfigManager
+            .addFeature(
+                "Clicked secret boxes",
+                "Draw a box on clicked secrets",
+                "General",
+                ConfigElement(
+                    "secretBox",
+                    ElementType.Switch(false)
+                )
             )
-        )
             .addFeatureOption(
-                "Box color: ",
+                "Box color",
                 ConfigElement(
                     "secretBox.color",
                     ElementType.ColorPicker(Color.red)
@@ -52,21 +52,32 @@ object ClickedSecretsBox : Feature(
 
             clickedSecrets = roomMap[new] ?: mutableListOf()
         }
+
         register<RenderEvent.World.Last> { event ->
             for (entry in clickedSecrets) {
-                Render3D.drawOutlinedBB(AABB(entry), color, event.context.consumers(), event.context.matrixStack())
+                Render3D.drawOutlinedBB(
+                    AABB(entry),
+                    color,
+                    event.context.consumers(),
+                    event.context.matrixStack()
+                )
             }
         }
+
         register<DungeonEvent.Secrets.Chest> { event ->
             clickedSecrets.add(event.blockPos)
         }
+
         register<DungeonEvent.Secrets.Essence> { event ->
             clickedSecrets.add(event.blockPos)
         }
+
         register<DungeonEvent.Secrets.Misc> { event ->
-            if(event.secretType == DungeonEvent.Secrets.Type.LEVER)
+            if (event.secretType == DungeonEvent.Secrets.Type.LEVER) {
                 clickedSecrets.add(event.blockPos)
+            }
         }
+
         register<DungeonEvent.End> {
             clickedSecrets.clear()
             roomMap.clear()
